0be0328222b3af39c8112010f8b7b1af06e510e2
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-08 17:08:01 -0500

    componente header con estilos mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.scss';
+import './styles/App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { } from '@fortawesome/free-solid-svg-icons';
-
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div className="App">
-
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "../styles/header.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUser, faXmark, faHouse, faMusic, faCartShopping, faLock, faUserPen } from '@fortawesome/free-solid-svg-icons';
+import Logo from "../Assets/logos/logo-vm.png";
+
+
+function Header() {
+  return (
+    <header className='header'>
+        <img className='img-logo' src={Logo} alt="logo VM" />
+        <div className='contenedor-menu-login'>
+            <div className='login'><FontAwesomeIcon icon={faUser} /> </div>
+            <FontAwesomeIcon className='hamburguer' icon={faBars} />   
+        </div>
+        <Navmobile></Navmobile>
+    </header>
+  )
+}
+
+
+function Navmobile() {
+    return(
+        <nav className='nav-movile'>
+            <ul className='nav-mobile-ul'>
+                <li className="nav-movile--li close"><FontAwesomeIcon className='icon-nav-close' icon={faXmark} /> </li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faHouse} />Inicio</li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faMusic} />Catalogo Musical</li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faCartShopping} />Tienda</li>
+                <li className='limitador'></li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faLock} />Admin</li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faUserPen} />Registrarse</li>
+                <li className="nav-movile--li"><FontAwesomeIcon className='icon-nav-mobile' icon={faUser} />Iniciar Sesión</li>
+                <li className="nav-movile--li"><img src={Logo} alt="logo-VM" /></li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Header
